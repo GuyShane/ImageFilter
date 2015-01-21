6b36c5a3968be02e28d2465b39a48f57711c955b
GuyShane <brasss@hotmail.com>
2015-01-21 11:26:05 -0700

    Changing MeanFilter to average colour channels, not just the whole number

--- src/com/imagefilter/MeanFilter.java
+++ src/com/imagefilter/MeanFilter.java
@@ -4,14 +4,17 @@ public class MeanFilter extends Filter {
 
 	@Override
 	public int filterCalculation(int[] window) {
-		return avg(window);
-	}
-	
-	private int avg(int []arr) {
-		int avg=0;
-		for (int num:arr) {
-			avg+=num/arr.length;
+		int r=0;
+		int g=0;
+		int b=0;
+		for (int num: window) {
+			r+=(num>>16)&0xff;
+			g+=(num>>8)&0xff;
+			b+=num&0xff;
 		}
-		return avg;
+		r/=window.length;
+		g/=window.length;
+		b/=window.length;
+		return (0xff<<24)|(r<<16)|(g<<8)|(b);
 	}
 }
